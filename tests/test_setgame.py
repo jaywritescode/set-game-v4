6271a2afe3a156b0fb1b6820621c71055929c2ff
Jay Harris <jaywritescode@users.noreply.github.com>
2022-06-27 12:18:47 -0400

    more tests

--- tests/test_setgame.py
+++ tests/test_setgame.py
@@ -94,6 +94,96 @@ def standard_deck():
     return deque(cards) 
 
 
+@pytest.fixture
+def larger_deck():
+    cards = [
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.RED, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.STRIPED, shape=Shape.OVAL, color=Color.GREEN, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.RED, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.STRIPED, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.OVAL, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.BLUE, number=Number.TWO), 
+        Card(shading=Shading.EMPTY, shape=Shape.DIAMOND, color=Color.BLUE, number=Number.THREE), 
+        Card(shading=Shading.SOLID, shape=Shape.DIAMOND, color=Color.GREEN, number=Number.TWO), 
+        Card(shading=Shading.SOLID, shape=Shape.SQUIGGLE, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.OVAL, color=Color.GREEN, number=Number.ONE), 
+        Card(shading=Shading.EMPTY, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.THREE), 
+        Card(shading=Shading.STRIPED, shape=Shape.SQUIGGLE, color=Color.RED, number=Number.THREE)
+    ]
+    return deque(cards)
+
+################################################
+# test that it deals the correct number of cards
+################################################
 def test_it_deals_twelve_cards(standard_deck):
     game = Game()
     game.cards = standard_deck
@@ -102,3 +192,36 @@ def test_it_deals_twelve_cards(standard_deck):
         assert_that(game.deal()).is_true()
         assert_that(game.board).is_length(12)
         assert_that(game.cards).is_length(69)
+
+
+def test_it_deals_eighteen_cards(larger_deck):
+    game = Game()
+    game.cards = larger_deck
+
+    with soft_assertions():
+        assert_that(game.deal()).is_true()
+        assert_that(game.board).is_length(18)
+        assert_that(game.cards).is_length(63)
+
+
+################################################
+# test that it adds players
+################################################
+def test_it_adds_players():
+    game = Game()
+    game.add_player('jeff')
+    game.add_player('ted')
+    game.add_player('ron')
+
+    with soft_assertions():
+        assert_that(game.players).contains_key('jeff')
+        assert_that(game.players).contains_key('ted')
+        assert_that(game.players).contains_key('ron')
+
+
+def test_it_cant_add_the_same_player_twice():
+    game = Game()
+    game.add_player('jeff')
+
+    with pytest.raises(ValueError):
+        game.add_player('jeff')
