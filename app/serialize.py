0e6191ce546ec5967525fca1ea211e5097ace481
Jay Harris <jaywritescode@users.noreply.github.com>
2022-07-09 11:25:20 -0400

    black

--- app/serialize.py
+++ app/serialize.py
@@ -7,8 +7,13 @@ from app.setgame import Card
 CardSchema = marshmallow_dataclass.class_schema(Card)()
 
 
-GameSchema = Schema.from_dict({
-    "board": fields.List(fields.Nested(CardSchema)),
-    "players": fields.Dict(keys=fields.Str(), values=fields.List(fields.List(fields.Nested(CardSchema)))),
-    "game_over": fields.Boolean()
-})
+GameSchema = Schema.from_dict(
+    {
+        "board": fields.List(fields.Nested(CardSchema)),
+        "players": fields.Dict(
+            keys=fields.Str(),
+            values=fields.List(fields.List(fields.Nested(CardSchema))),
+        ),
+        "game_over": fields.Boolean(),
+    }
+)
