63fbd61c6fcd15a5ccd7e3257acd0423e86f5268
Jay Harris <jaywritescode@users.noreply.github.com>
2022-07-14 10:25:17 -0400

    set up initial state

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,31 @@
-import { useEffect } from "react";
+import { useEffect, useReducer } from "react";
 import useWebsocket, { ReadyState } from "react-use-websocket";
 import generate from "project-name-generator";
 import logo from './logo.svg';
 import './App.css';
 
+const GameStates = Object.freeze({
+  WAITING_TO_START: 0,
+  IN_PROGRESS: 1,
+  GAME_OVER: 2,
+});
+
 const JOIN_GAME = 'join_game';
+
 const socketUrl = "ws://localhost:3001/ws";
 
 const playerName = generate().dashed;
 
+const reducer = (state, action) => state;
+
+
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    board: [],
+    players: [],
+    game_state: GameStates.WAITING_TO_START,
+  });
+
   const { sendJsonMessage, readyState } = useWebsocket(socketUrl, {
     onOpen: (e) => {
       console.log('[useWebsocket:onOpen] ', e);
