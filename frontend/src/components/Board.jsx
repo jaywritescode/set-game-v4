98ab87c8a601399e03ce234f5c175e5086dbe5c1
Jay Harris <jaywritescode@users.noreply.github.com>
2022-08-12 14:42:32 -0400

    handle state change when card is clicked

--- frontend/src/components/Board.jsx
+++ frontend/src/components/Board.jsx
@@ -1,13 +1,36 @@
-import React from "react";
+import React, { useReducer } from "react";
 import Card from "./Card";
 import styles from "./Board.module.css";
+import * as R from "ramda";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "deselectCard":
+      return R.without([action.payload], state);
+    case "selectCard":
+      return [...state, action.payload];
+    default:
+      throw new Error();
+  }
+}
 
 export default function Board(props) {
+  const [state, dispatch] = useReducer(reducer, []);
+
   const { cards } = props;
+
+  const onCardClicked = (card) => {
+    console.log('onCardClicked: ', card);
+
+    const type = state.find(R.equals(card)) ? "deselectCard" : "selectCard";
+    dispatch({ type, payload: card });
+  };
 
   return (
     <div className={styles.container}>
-      {cards.map(card => (<Card {...card} />))}
+      {cards.map((card) => (
+        <Card {...card} onClick={() => onCardClicked(card)} key={`${card.number}-${card.color}-${card.shading}-${card.shape}`} />
+      ))}
     </div>
   );
 }
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -3,9 +3,15 @@ import * as R from "ramda";
 import styles from "./Card.module.css";
 
 export default function Card(props) {
-  const { color, number, shading, shape } = props;
+  const { color, number, shading, shape, onClick } = props;
 
   const imageName = [number, color, shading, shape].map(R.toLower).join("-");
 
-  return <div className={styles.card} style={{ backgroundImage: `url(/cards/${imageName}.png)` }} />;
+  return (
+    <div
+      className={styles.card}
+      style={{ backgroundImage: `url(/cards/${imageName}.png)` }}
+      onClick={onClick}
+    />
+  );
 }
