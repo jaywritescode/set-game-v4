52eaebf94937a206fb9ec351d76664fcae894e4d
Jay Harris <jaywritescode@users.noreply.github.com>
2022-09-26 16:31:35 -0400

    clean up player name form

--- frontend/src/components/PlayerNameForm.jsx
+++ frontend/src/components/PlayerNameForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import Stack from 'react-bootstrap/Stack';
 
 export default function PlayerNameForm(props) {
 
@@ -9,12 +10,12 @@ export default function PlayerNameForm(props) {
   const [name, setName] = useState('');
 
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="playerName">
-        <Form.Label>Your Name</Form.Label>
-        <Form.Control type="text" onChange={(e) => setName(e.currentTarget.value)}/>
-        <Button variant="primary" onClick={() => onClickJoinGame(name)}>Join Game</Button>
-      </Form.Group>
-    </Form>
+    <Stack direction="horizontal" gap={3}>
+      <Form.Control 
+        type="text" 
+        placeholder="Your name..."
+        onChange={(e) => setName(e.currentTarget.value)} />
+      <Button variant="primary" onClick={() => onClickJoinGame(name)}>Join Game</Button>
+    </Stack>
   );
 }
